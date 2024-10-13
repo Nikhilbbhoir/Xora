38b1cdbad2a2d1805c2dddb1fa16865f0f2da6f5
Nikhil <Nikhilbhoir3716@gmail.com>
2024-10-13 16:56:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Header from './sections/Header'
+import Hero from './sections/Hero'
+import Features from './sections/Features'
+import Pricing from './sections/Pricing'
+import Faq from './sections/Faq'
+import Testimonials from './sections/Testimonials'
+import Downloads from './sections/Downloads'
+import Footer from './sections/Footer'
+
+const App = () => {
+  return (
+    <main className="overflow-hidden">
+      <Header />
+      <Hero />
+      <Features />
+      <Pricing />
+      <Faq />
+      <Testimonials />
+      <Downloads />
+      <Footer />
+    </main>
+  )
+}
+
+export default App
